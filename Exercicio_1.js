4a9df0924635cea4beb2b3276a807960fd662d17
PedraForte2406 <163930513+PedraForte2406@users.noreply.github.com>
2025-02-20 21:02:38 -0300

    Update Exercicio_1.js

--- Exercicio_1.js
+++ Exercicio_1.js
@@ -1,1 +1,14 @@
-alert
+function ClassificarIdade(idade) {
+    if (!isNaN(idade) && idade < 0) {
+        console.log("Idade inválida.");
+    } else if (idade < 12) {
+        console.log("Criança");
+    } else if (idade < 17) {
+        console.log("Adolecente");
+    } else if (idade < 59) {
+        console.log("Adulto");
+    } else if (idade > 60) {
+        console.log("Você é um idoso.");
+    }
+}
+ClassificarIdade("sim")
